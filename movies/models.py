c0e97c541faf34e8ab7a479d551c6929904b0ed8
sebastianvdn <sebastian.van.den.noortgaete@gmail.com>
2020-09-13 18:16:10 +0200

    create model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class WatchList(models.Model):
+    movie_title = models.CharField(max_length=100)
+    movie_id = models.CharField(max_length=9, db_index=True)
+    movie_genre = models.CharField(max_length=255)
+    movie_actors = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, related_name='watchlist', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} wants to watch {self.movie_title}'
